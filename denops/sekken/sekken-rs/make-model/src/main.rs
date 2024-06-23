3695b3ab0f5bcaf868e7b0ad45d92eb981e0d9d2
Shinnosuke Sawada <shin@warashi.dev>
2024-06-23 17:48:10 +0900

    feat: update model

---
+++ denops/sekken/sekken-rs/make-model/src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{BufWriter, Write};
+
+use anyhow::{Context, Result};
+
+use sekken_model::NormalModel;
+
+fn main() -> Result<()> {
+    let input = std::io::stdin();
+    let input = std::io::BufReader::new(input);
+    let mut input = utf8_read::Reader::new(input);
+    let mut pre: Option<char> = None;
+
+    let mut model = NormalModel::new();
+
+    for ch in input.into_iter().flatten() {
+
+        model.increment_unigram_cost(ch);
+
+        if let Some(p) = pre {
+            model.increment_bigram_cost(p, ch);
+        }
+
+        pre = Some(ch);
+    }
+    let model = model;
+
+    let output = std::io::stdout();
+    let mut output = BufWriter::new(output);
+
+    model
+        .compact()
+        .save(&mut output)
+        .context("Failed to save model")?;
+
+    output.flush().context("Failed to flush output")?;
+
+    Ok(())
+}
