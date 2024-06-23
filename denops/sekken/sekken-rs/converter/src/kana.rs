c5b04237b2eb29d7e651a6569ad21ec4f5617c85
Shinnosuke Sawada <shin@warashi.dev>
2024-06-24 08:48:06 +0900

    feat: implement converter

---
+++ denops/sekken/sekken-rs/converter/src/kana.rs
@@ -0,0 +1,67 @@
+mod table;
+
+use daachorse::{DoubleArrayAhoCorasick, DoubleArrayAhoCorasickBuilder, MatchKind};
+
+#[derive(Clone)]
+pub struct KanaTable {
+    table: DoubleArrayAhoCorasick<usize>,
+    values: Vec<String>,
+}
+
+impl Default for KanaTable {
+    fn default() -> Self {
+        let Some(t) = KanaTable::new(table::default()) else {
+            unreachable!()
+        };
+        return t;
+    }
+}
+
+impl sekken_lattice::Converter for KanaTable {
+    fn convert(self: &Self, word: &String) -> Vec<String> {
+        return vec![
+            self.roman2kana(word.clone()),
+            self.roman2kana(word[0..word.len() - 1].to_string()),
+        ];
+    }
+}
+
+impl KanaTable {
+    pub fn new<I>(patvals: I) -> Option<KanaTable>
+    where
+        I: IntoIterator<Item = (String, String)>,
+    {
+        let patvals = patvals.into_iter().collect::<Vec<_>>();
+        let values = patvals
+            .clone()
+            .into_iter()
+            .map(|(_, val)| val)
+            .collect::<Vec<_>>();
+        let table = patvals
+            .clone()
+            .into_iter()
+            .enumerate()
+            .map(|(i, (pat, _))| (pat, i))
+            .collect::<Vec<_>>();
+        let table = DoubleArrayAhoCorasickBuilder::new()
+            .match_kind(MatchKind::LeftmostLongest)
+            .build_with_values(table)
+            .ok()?;
+
+        Some(KanaTable { table, values })
+    }
+
+    pub fn roman2kana(&self, roman: String) -> String {
+        let mut it = self
+            .table
+            .leftmost_find_iter(roman.clone())
+            .collect::<Vec<_>>();
+        it.reverse();
+
+        let mut kana = roman.clone();
+        for m in it {
+            kana.replace_range(m.start()..m.end(), self.values[m.value()].as_str());
+        }
+        kana
+    }
+}
