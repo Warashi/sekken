149c11b84540165067940a5b20203a9c731ac1ad
Shinnosuke Sawada <shin@warashi.dev>
2024-06-24 08:54:00 +0900

    feat: implement dict core

--- denops/sekken/sekken-rs/dict/src/lib.rs
+++ denops/sekken/sekken-rs/dict/src/lib.rs
@@ -1,1 +1,12 @@
+use std::collections::BTreeMap;
 
+#[derive(Default, Clone, Debug)]
+pub struct Dict {
+    inner: BTreeMap<String, Vec<String>>,
+}
+
+impl sekken_lattice::Dict for Dict {
+    fn get(self: &Self, word: &String) -> Vec<String> {
+        return self.inner.get(word).unwrap_or(&Vec::new()).to_vec();
+    }
+}
