3ef54c1846fd4b465c4297b514e5db045d95663a
Shinnosuke Sawada <shin@warashi.dev>
2024-06-25 18:43:01 +0900

    feat(dict): implement reading skk json format

--- denops/sekken/sekken-rs/dict/src/lib.rs
+++ denops/sekken/sekken-rs/dict/src/lib.rs
@@ -1,4 +1,10 @@
 use std::collections::BTreeMap;
+use std::io::Read;
+
+use anyhow::Context as _;
+use anyhow::Result;
+
+use serde::{Deserialize, Serialize};
 
 #[derive(Default, Clone, Debug)]
 pub struct Dict {
@@ -7,6 +13,31 @@ pub struct Dict {
 
 impl sekken_lattice::Dict for Dict {
     fn get(self: &Self, word: &String) -> Vec<String> {
-        return self.inner.get(word).unwrap_or(&Vec::new()).to_vec();
+        return self.inner.get(word).unwrap_or(&Vec::new()).clone();
+    }
+}
+
+impl Dict {
+    pub fn from_skk_json<R: Read>(r: R) -> Result<Self> {
+        #[derive(Serialize, Deserialize, Debug, Clone)]
+        struct SKKDictJSON {
+            pub okuri_ari: BTreeMap<String, Vec<String>>,
+            pub okuri_nasi: BTreeMap<String, Vec<String>>,
+        }
+
+        let mut dict: SKKDictJSON = serde_json::from_reader(r).context("parse json dictionary")?;
+
+        let mut out = Dict::default();
+        out.inner.append(&mut dict.okuri_ari);
+
+        for (k, v) in out.inner.iter_mut() {
+            if let Some(mut n) = dict.okuri_nasi.remove(k) {
+                v.append(&mut n)
+            }
+        }
+
+        out.inner.append(&mut dict.okuri_nasi);
+
+        return Ok(out);
     }
 }
